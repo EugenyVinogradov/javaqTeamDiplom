cff7465944a27955b1a7f115c91590caf957a45a
Агабекян Эдгар Артемович <agabekyan.e@milandr.ru>
2022-07-02 15:55:38 +0500

    Fixed playedTime RuntimeExeption sumGenre mostPlayerByGenre

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -6,6 +6,22 @@ import java.util.Map;
 public class Player {
     private String name;
 
+    /**
+     * информация о том, в какую игру сколько часов было сыграно
+     * ключ - игра
+     * значение - суммарное количество часов игры в эту игру
+     */
+
+    private Map<Game, Integer> playedTime = new HashMap<>();
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -18,27 +34,15 @@ public class Player {
         this.playedTime = playedTime;
     }
 
-    /**
-     * информация о том, в какую игру сколько часов было сыграно
-     * ключ - игра
-     * значение - суммарное количество часов игры в эту игру
-     */
-    private Map<Game, Integer> playedTime = new HashMap<>();
-
-    public Player(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     /**
      * добавление игры игроку
      * если игра уже была, никаких изменений происходить не должно
      */
     public void installGame(Game game) {
-        playedTime.put(game, 0);
+        if (!playedTime.containsKey(game)) {
+            playedTime.put(game, 0);
+        }
     }
 
     /**
@@ -50,10 +54,13 @@ public class Player {
      */
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
+        if (hours <= 0) {
+            throw new RuntimeException("Игровое время не может быть отрицательным или равным нулю!");
+        }
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.get(game) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("У игрока " + this.name + " игра " + game + " не установлена!");
         }
         return playedTime.get(game);
     }
@@ -67,8 +74,6 @@ public class Player {
         for (Game game : playedTime.keySet()) {
             if (game.getGenre().equals(genre)) {
                 sum += playedTime.get(game);
-            } else {
-                sum = 0;
             }
         }
         return sum;
@@ -79,6 +84,17 @@ public class Player {
      * Если в игры этого жанра не играли, возвращается null
      */
     public Game mostPlayerByGenre(String genre) {
+        int max = 0;
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre) && playedTime.get(game) > max) {
+                max = playedTime.get(game);
+            }
+        }
+        for (Game game : playedTime.keySet()) {
+            if (max == playedTime.get(game)) {
+                return game;
+            }
+        }
         return null;
     }
 }
